571d4f82f2f303e3f776ea963a90f590187abbb4
hija-happy <hijahappy2004@gmail.com>
2025-03-05 14:50:06 +0530

    Added Responsive MenuBar

--- js/function.js
+++ js/function.js
@@ -7,3 +7,24 @@ function closeSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display='none'
 }
+
+const navMenuBtn = document.querySelector('.menu');
+const navList = document.querySelector('.nav-list');
+
+    navMenuBtn.addEventListener('click',function(){
+        navList.classList.toggle('active')
+    });
+
+document.addEventListener('click',function(event){
+    if(!navList.contains(event.target) && !navMenuBtn.contains(event.target))
+        navList.classList.remove('active');
+});
+
+const navListItem = document.querySelectorAll('.nav-list li');
+navListItem.forEach(item => {
+    item.addEventListener('click',function(){
+        const link = this.querySelector('a');
+        if(link)
+            window.location.href =link.getAttribute('href');
+    })
+})
